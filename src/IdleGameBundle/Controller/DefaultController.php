2d766954489d8823321f92a7f5c77e783b9ebcbf
Merias <hugo.merias@lapiscine.pro>
2018-07-11 09:45:00 +0200

    modifications 2

--- src/IdleGameBundle/Controller/DefaultController.php
+++ src/IdleGameBundle/Controller/DefaultController.php
@@ -10,4 +10,17 @@ class DefaultController extends Controller
     {
         return $this->render('@IdleGame/Default/index.html.twig');
     }
+
+    public function loginAction()
+    {
+        return $this->render('@IdleGame/Default/login.html.twig');
+    }
+
+    public function transitionAction($login,$password)
+    {
+        return $this->render('@IdleGame/Default/transition.html.twig',array(
+            'login'=> $login,
+            'password'=> $password,
+        ));
+    }
 }
